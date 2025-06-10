2423232407b4af03547c699d8d73ea0e5cacfc53
allen <boseallen192@gmail.com>
2025-06-10 22:37:45 +0530

    init commit

---
+++ native-client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace native_client;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "native-client" };
+	}
+}
